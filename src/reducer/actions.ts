467f6639acb5c0bde4ae0270ba5a3276a661c678
Дмитрий <mitya-kudr@mail.ru>
2022-06-25 14:31:37 +0300

    Initialize project using Create React App

---
+++ src/reducer/actions.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-shadow
+enum Actions {
+    SetWeb3 = "SET_WEB3",
+    SetAccount = "SET_ACCOUNT",
+    SetChain = "SET_CHAIN",
+}
+
+export default Actions;
---
+++ src/reducer/index.ts
@@ -0,0 +1,48 @@
+import Web3 from "web3";
+import Actions from "./actions";
+import { NetworkType } from "../utils/network";
+
+export type ActionType =
+    | {
+          type: Actions.SetWeb3;
+          payload: Web3;
+      }
+    | {
+          type: Actions.SetAccount;
+          payload?: string;
+      }
+    | {
+          type: Actions.SetChain;
+          payload?: NetworkType;
+      };
+
+export interface ReducerState {
+    web3: Web3;
+    account?: string;
+    chain?: NetworkType;
+}
+
+const reducer = (state: ReducerState, action: ActionType) => {
+    const { type, payload } = action;
+    switch (type) {
+        case Actions.SetWeb3:
+            return {
+                ...state,
+                web3: payload,
+            };
+        case Actions.SetAccount:
+            return {
+                ...state,
+                account: payload,
+            };
+        case Actions.SetChain:
+            return {
+                ...state,
+                chain: payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
