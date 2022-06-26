19b99e5dea23d91ee1fc38fb46b5cdba01a1a92b
Mac <>
2022-06-26 18:11:07 +0300

    My tasks implemented.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,7 +6,7 @@ const responseWrapper = async (f: Promise<any>) => {
     return response.data;
 };
 
-const ENDPOINT = "https://79003582163d80.lhrtunnel.link";
+const ENDPOINT = "https://3a87c9864c1f99.lhrtunnel.link";
 
 export const createTask = async (task: CreateTaskType) => responseWrapper(axios.post(`${ENDPOINT}/createTask`, task));
 
